77c4ab9c2963d9cc7e997034adabe3648a303f3c
Gonzalo Garcia <gonzagarcia14@gmail.com>
2023-05-14 18:25:19 +0200

    Finished part 2 of ex1

--- ex1/ex1.go
+++ ex1/ex1.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 )
 
 type dataFrame struct {
@@ -12,7 +13,7 @@ type dataFrame struct {
 	userAnswer string
 }
 
-func Ex1(path string) {
+func Ex1(path string, timeOut int) {
 
 	var correctCount int = 0
 	var questionCount int = 0
@@ -29,6 +30,15 @@ func Ex1(path string) {
 		fmt.Println(err)
 	}
 
+	fmt.Println("Press enter to start.")
+	fmt.Scanln()
+	go func() {
+		time.Sleep(time.Second * time.Duration(timeOut))
+		fmt.Println("Time's up!")
+		fmt.Printf("You got %d out of %d questions right.\n", correctCount, questionCount)
+		os.Exit(0)
+	}()
+
 	for _, row := range problems {
 
 		var i string
@@ -39,19 +49,17 @@ func Ex1(path string) {
 		}
 
 		fmt.Println(df.problem)
+		questionCount = questionCount + 1
 		fmt.Scan(&i)
 
 		df.userAnswer = i
 		if df.userAnswer == df.answer {
 			fmt.Print("Your Answer was correct \n")
 			correctCount = correctCount + 1
-			questionCount = questionCount + 1
 		} else {
 			fmt.Print("Your Answer was not correct \n")
-			questionCount = questionCount + 1
 		}
 
 	}
 
-	fmt.Printf("You got %d out of %d questions right.\n", correctCount, questionCount)
 }
--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 func main() {
 
 	var exNumber = flag.Int("n", 1, "Sets which exercise is executed.")
+	var timeOut = flag.Int("timeOut", 30, "Sets time limit for exercise in seconds.")
 	var path = flag.String("pathEx1", "ex1/problems.csv", "Set path to CSV file with questions for Exercise 1.")
 	flag.Parse()
 
 	if *exNumber == 1 {
-		ex1.Ex1(*path)
+		ex1.Ex1(*path, *timeOut)
 	}
 
 }
