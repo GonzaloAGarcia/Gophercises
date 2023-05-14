d7b06ad5e8ab4ab195cb1654f17288038eeee4d1
Gonzalo Garcia <gonzagarcia14@gmail.com>
2023-05-14 18:55:44 +0200

    Rename

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"flag"
 
-	"gophercises.gonzalogarcia.de/ex1"
+	ex1 "gophercises.gonzalogarcia.de/quizGame"
 )
 
 func main() {
 
 	var exNumber = flag.Int("n", 1, "Sets which exercise is executed.")
 	var timeOut = flag.Int("timeOut", 30, "Sets time limit for exercise in seconds.")
-	var path = flag.String("pathEx1", "ex1/problems.csv", "Set path to CSV file with questions for Exercise 1.")
+	var path = flag.String("pathEx1", "quizGame/problems.csv", "Set path to CSV file with questions for Exercise 1.")
 	flag.Parse()
 
 	if *exNumber == 1 {
